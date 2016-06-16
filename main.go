8a9e10479b9e314911c38755f7de2bd71f1866d2
matthework <matthework@hotmail.com>
2016-06-16 18:45:08 +1200

    change port

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 
 	router := NewRouter()
+	port := os.Getenv("PORT")
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":" + port, router))
 }
